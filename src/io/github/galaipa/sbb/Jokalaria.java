6c3372e146085a2a864732bdeffcd73e227c8fd0
MatteCarra <matte.carra@gmail.com>
2015-12-27 01:34:50 +0100

    Removed PlayerPoints and reformat code

--- src/io/github/galaipa/sbb/Jokalaria.java
+++ src/io/github/galaipa/sbb/Jokalaria.java
@@ -1,4 +1,3 @@
-
 package io.github.galaipa.sbb;
 
 import org.bukkit.Location;
@@ -10,102 +9,116 @@ import org.bukkit.inventory.ItemStack;
 
 
 public class Jokalaria {
+    public Player Jokalaria;
+    ItemStack[] armor;
+    ItemStack[] inv;
     private int id;
     private Arena arena;
     private Player p;
     private int point;
     private Cuboid cuboid;
-    private Location spawnpoint,startLoc;
-    public Player Jokalaria;
+    private Location spawnpoint, startLoc;
     private World world;
     private int type;
     private byte data;
     private int exp;
     public Jokalaria(Player p2, int arenaID, Location l) {
-	p = p2;
+        p = p2;
         point = 0;
         arena = ArenaManager.getManager().getArena(arenaID);
-        id = arena.getPlayers().size()+1;
+        id = arena.getPlayers().size() + 1;
         inv = p.getInventory().getContents();
-        armor= p.getInventory().getArmorContents();
+        armor = p.getInventory().getArmorContents();
         p.getInventory().setArmorContents(null);
         p.getInventory().clear();
         startLoc = l;
         exp = p.getLevel();
-	}
-    ItemStack[] armor;
-    ItemStack[] inv;
-    public int getID(){
+    }
+
+    public int getID() {
         return id;
     }
-    public void addRegion(Cuboid c){
-   //  cuboid = new Cuboid(l1,l2);
-    cuboid = c;
-   //  world = l1.getWorld();
-     world = c.getWorld();
-        for (Block block : cuboid.getFace(Cuboid.CuboidDirection.Down)){
+
+    public void addRegion(Cuboid c) {
+        //  cuboid = new Cuboid(l1,l2);
+        cuboid = c;
+        //  world = l1.getWorld();
+        world = c.getWorld();
+        for (Block block : cuboid.getFace(Cuboid.CuboidDirection.Down)) {
             data = block.getData();
             type = block.getTypeId();
             break;
-         }
-    }
-    public void resetArenas(){
-        for (Block block : cuboid){
-                block.setType(Material.AIR);
         }
-        setGround(type,data);
     }
-    public void setGround(int type,byte data){
-        for (Block block : cuboid.getFace(Cuboid.CuboidDirection.Down)){
+
+    public void resetArenas() {
+        for (Block block : cuboid) {
+            block.setType(Material.AIR);
+        }
+        setGround(type, data);
+    }
+
+    public void setGround(int type, byte data) {
+        for (Block block : cuboid.getFace(Cuboid.CuboidDirection.Down)) {
             block.setTypeIdAndData(type, data, true);
         }
     }
-    public Player getPlayer(){
+
+    public Player getPlayer() {
         return p;
     }
-    public World getWorld(){
+
+    public World getWorld() {
         return world;
     }
-    public Cuboid getCuboid(){
+
+    public Cuboid getCuboid() {
         return cuboid;
     }
 
-    public int getPoint(){
+    public int getPoint() {
         return point;
     }
-    public void addPoint(int p){
+
+    public void addPoint(int p) {
         point = point + p;
     }
-    public Location getSpawnPoint(){
+
+    public Location getSpawnPoint() {
         Location v = cuboid.getCenter();
         spawnpoint = v;
         return spawnpoint;
     }
-    public Location getPreSpawn(){
+
+    public Location getPreSpawn() {
         return startLoc;
     }
-    public void addPlayer(Player p){
+
+    public void addPlayer(Player p) {
         Jokalaria = p;
-        }
-    public void removePlayer(Player p){
+    }
+
+    public void removePlayer(Player p) {
         Jokalaria = null;
-        }
-    public String getPlayerString(){
+    }
+
+    public String getPlayerString() {
         return p.getName();
-        }
-    public void returnInv(){
+    }
+
+    public void returnInv() {
         p.getInventory().setContents(inv);
         p.getInventory().setArmorContents(armor);
         p.setLevel(exp);
     }
-    public Boolean checkPlayer(Player p){
+
+    public Boolean checkPlayer(Player p) {
         Player pa = Jokalaria;
-        if(p == pa ){
+        if (p == pa) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
-        }
     }
+}
 
--- src/io/github/galaipa/sbb/Winners.java
+++ src/io/github/galaipa/sbb/Winners.java
@@ -1,30 +1,28 @@
-
 package io.github.galaipa.sbb;
-
 
 
 public class Winners implements Comparable<Winners> {
     Jokalaria team;
     int score;
 
-    public Winners(Jokalaria name, int score){
+    public Winners(Jokalaria name, int score) {
         this.team = name;
         this.score = score;
     }
 
-    public int compareTo(Winners user){
+    public int compareTo(Winners user) {
         return user.getScore() - this.getScore();
     }
 
-    public Jokalaria getName(){
+    public Jokalaria getName() {
         return team;
     }
 
-    public int getScore(){
+    public int getScore() {
         return score;
     }
 
-    public String toString(){
+    public String toString() {
         return "Name: " + team + " Score: " + score;
     }
 }
